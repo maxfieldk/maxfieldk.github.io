563069757991ff4dd778782812c4efbe80f9c162
maxfieldk <44215152+maxfieldk@users.noreply.github.com>
2022-02-25 15:55:08 -0500

    af

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,3 +81,12 @@
 		}
 
 })(jQuery);
+
+$(".image.fit img").click(function(){
+	$("#full-image").attr("src", $(this).attr("src"));
+	$('#image-viewer').show();
+  });
+  
+  $("#image-viewer .close").click(function(){
+	$('#image-viewer').hide();
+  });
